029d0a8ab56674333cd0d883e70196a0724e109a
Meg Wilde <meg.c.wilde@gmail.com>
2023-10-30 07:31:17 +0000

    adding helloWorld file update where user can input what they want to print to the console

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,3 @@
 //console.log("Hello World!"); //program to print Hello World to the console
-
-console.log("Git is Awesome!"); //change to a program which prints Git is Awesome!
+//console.log("Git is Awesome!"); //change to a program which prints Git is Awesome!
+console.log(prompt("What do you want to output?")); //program to print what the user inputs to the prompt
